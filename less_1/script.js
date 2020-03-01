a06f414377bd22827f058bf29eac11c483ac0bff
makc <admin@mail.ua>
2020-03-01 22:09:54 +0200

    Это наш первый комит

---
+++ less_1/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+//console.log(second);
+
+var first = 1;
+let second = 2;
+const pi = 3.14;
+
+console.log(second);
+console.log('string'*4);
+
+let persone = {
+    name : "John",
+    age : 16,
+    married : false
+};
+
+/* 
+console.log(persone["name"]);
+
+let answer = prompt("Ты здесь?","Да");
+console.log(typeof(answer)); */
+
+
+let incr = 10,
+    decr = 10;
+
+console.log(++incr);
+console.log(--decr);
