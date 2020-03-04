20c04b86be1c94c402893deaf1f821ecf7771cef
makc <makc.baykov@gamil.com>
2020-03-05 00:03:04 +0200

    первая домашка

---
+++ homework/script.js
@@ -0,0 +1,39 @@
+'use strict';
+
+let money,
+    time,
+    optionalExpenses,
+    income;
+
+money = prompt("Ваш бюджет на месяц ?","");
+time = prompt(" Введите дату в формате YYYY-MM-DD");
+
+
+let q1  = prompt("Введите обязательную статью 1 расходов в этом месяце","");
+let q2  = prompt("Во сколько обойдется?","");
+let q3  = prompt("Введите обязательную статью 2 расходов в этом месяце","");
+let q4  = prompt("Во сколько обойдется?","");
+
+console.log(q1);
+
+let appData = {
+    money : money,
+    time : time,
+    expenses : {
+        "q1" : q2,
+        "q3" : q4
+    },
+    optionalExpenses,
+    income
+};
+
+
+for (var key in appData.expenses) {
+    // этот код будет вызван для каждого свойства объекта
+    // ..и выведет имя свойства и его значение
+  
+    console.log( "Ключ: " + key + " значение: " + appData.expenses[key] );
+}
+
+
+alert ("Бюджет на один день - "+ money/30);
