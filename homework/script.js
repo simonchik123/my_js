b4279b6c62a613260c067a68d143d6f690117e2a
makc <makc.baykov@gamil.com>
2020-03-10 22:13:52 +0200

    оптимизировал цикл и условия

--- homework/script.js
+++ homework/script.js
@@ -1,29 +1,30 @@
 'use strict';
 
-let money = prompt("Ваш бюджет на месяц ?",""),
+let money = +prompt("Ваш бюджет на месяц ?",""),
     time = prompt(" Введите дату в формате YYYY-MM-DD","");
 
 
 
-let q1  = prompt("Введите обязательную статью 1 расходов в этом месяце","");
-let q2  = prompt("Во сколько обойдется?","");
-let q3  = prompt("Введите обязательную статью 2 расходов в этом месяце","");
-let q4  = prompt("Во сколько обойдется?","");
-
-console.log(q1);
-
 let appData = {
     money : money,
     time : time,
-    expenses : {
-        [q1] : q2,
-        [q3] : q4
-    },
+    expenses : {},
     optionalExpenses : {},
     income :[],
     savings: false
 };
 
+for (let i = 1;i < 3; i++){
+    let q1  = prompt("Введите обязательную статью расходов в этом месяце","");
+    let q2  = +prompt("Во сколько обойдется?","");
+
+    if ((typeof(q1)==='string') && (typeof(q1)!= null) && (typeof(q2)!= null)
+        && q1 != '' && q2 !='' && q1.length < 50) {
+    appData.expenses[q1] = q2;
+    } else {
+        i--;
+    }
+}
 
 for (var key in appData.expenses) {
     // этот код будет вызван для каждого свойства объекта
@@ -32,6 +33,16 @@ for (var key in appData.expenses) {
     console.log( "Ключ: " + key + " значение: " + appData.expenses[key] );
 }
 
-console.log(appData.expenses.еда);
+appData.moneyForDay = appData.money / 30;
 
-alert ("Бюджет на один день - "+ money/30);
+
+alert ("Бюджет на один день - "+ appData.moneyForDay);
+
+if(appData.moneyForDay <100){
+    console.log(" Минимальный уровень достатка");
+} else if (100 < appData.moneyForDay < 2000){
+    console.log(" Средний уровень достатка");
+} else if (appData.moneyForDay > 2000){
+    console.log(" Высокий уровень достатка");
+} else { console.log(" Произошла ошибка");
+}
