8b906bbccf4b22b31e9c14b43edbdce39954720d
makc <makc.baykov@gamil.com>
2020-03-17 20:53:36 +0200

    Замыкания

--- less_1/script.js
+++ less_1/script.js
@@ -46,3 +46,17 @@ function get_arr(arr=[]){
   }
 
   console.log(get_arr());
+  
+  function createCounter() {
+       let counter = 0;
+       const myFunction = function() {
+         counter = counter + 1;
+         return counter;
+       };
+       return myFunction;
+     }
+     const increment = createCounter();
+    const c1 = increment();
+    const c2 = increment();
+    const c3 = increment();
+    console.log('example increment', c1, c2, c3);
