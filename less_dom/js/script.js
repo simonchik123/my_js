c5dfc173924dc3feed83fa9192f566af3abd55a1
Дачная20 <admin@mail.ua>
2020-04-20 15:17:37 +0300

    dom_1

---
+++ less_dom/js/script.js
@@ -0,0 +1,14 @@
+let box = document.getElementById('title'),
+    li = document.getElementsByTagName('li'),
+    column = document.getElementsByClassName('column'),
+    menu_item = document.querySelectorAll('.menu .menu-item'),
+    column_s = document.querySelector('.column');
+
+
+
+console.log(box);
+console.log(li);
+console.log(li[0]);
+console.log(column[1]);
+console.log(menu_item);
+console.log(column_s);
