b23fd299ae904ee39b65a5e43f2ee0c5c9e06f80
ws421 <Baykov_MF@apk-invest.com>
2020-04-30 13:56:33 +0300

    17-7

---
+++ homework/src/js/main.js
@@ -0,0 +1,22 @@
+let bStart = document.getElementById('start'),
+    budget = document.querySelector('.budget-value'),
+    daybudget = document.querySelector('.daybudget-value'),
+    level = document.querySelector('.level-bvalue'),
+    expenses = document.querySelector('.expenses-value'),
+    optionalexpenses = document.querySelector('.optionalexpenses-value'),
+    income = document.querySelector('.income-value'),
+    monthsavings = document.querySelector('.monthsavings-value'),
+    yearsavings = document.querySelector('.yearsavings-value'),
+    year = document.querySelector('.year .year-value'),
+    month = document.querySelector('.year .month-value'),
+    day = document.querySelector('.year .day-value');
+
+let inputsExpenses = document.querySelectorAll('.expenses-item'),
+    optionalExpensesBtn = document.querySelector('.optionalexpenses-btn'),
+    countBudgetBtn = document.querySelector('.count-budget-btn'),
+    optionalExpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+    chooseIncomeLabel = document.querySelector('#income'),
+    savings = document.querySelector('#savings'),
+    sum = document.querySelector('#sum'),
+    percent = document.querySelector('#percent');
+console.log(chooseIncomeLabel);
