846f7d657b72a8b474c035303ac17a3b6505110e
omen <makc.baykov@gamil.com>
2020-07-28 15:25:45 +0300

    + less_32_slider

--- less_19/js/script.js
+++ less_19/js/script.js
@@ -137,7 +137,7 @@ window.addEventListener('DOMContentLoaded', function () {
             form.appendChild(statusMessage);
             let formData = new FormData(form);
 
-            function postData(data) {
+            function postData() {
 
                 return new Promise(function (resolve, reject) {
 
@@ -170,7 +170,7 @@ window.addEventListener('DOMContentLoaded', function () {
                 }
             }
 
-            postData(formData)
+            postData()
                 .then(()=>statusMessage.innerHTML = message.loading)
                 .then(()=>statusMessage.innerHTML = message.success)
                 .catch(()=>statusMessage.innerHTML = message.failure)
@@ -181,4 +181,62 @@ window.addEventListener('DOMContentLoaded', function () {
     }
 
     sendForm(form);
+
+    // slider
+
+    let sliderIndex = 1,
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
+    
+    showSlides(sliderIndex);
+
+
+    function showSlides(n){
+
+        if (n >= slides.length){
+            sliderIndex = 0;
+        }else if (n < 0){
+            sliderIndex = slides.length -1;
+        }
+        
+        slides.forEach(element => {
+            element.style.display = 'none';  
+        });
+        dots.forEach(element => {
+            element.classList.remove('dot-active');
+        });
+        slides[sliderIndex].style.display = 'block';
+        dots[sliderIndex].classList.add('dot-active');
+    }
+    
+    function plusSlide(n){
+        showSlides(sliderIndex +=n);
+    }
+
+    function currentSlide(n){
+        showSlides(sliderIndex = n);
+    }
+
+    prev.addEventListener('click', function(){
+        plusSlide(-1);
+    });
+    next.addEventListener('click', function(){
+        plusSlide(1);
+    });
+
+    dotsWrap.addEventListener('click', function(event){
+        let target = event.target;
+        if (target && target.classList.contains('dot')) {
+            for (let i = 0; i < dots.length; i++) {
+                if (target == dots[i]) {
+                    currentSlide(i);
+                    
+                }
+            }
+        }
+    });
+    
 });
