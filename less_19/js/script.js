adb8fd2b4606f7a232ff739a80075a5fd78aee46
omen <makc.baykov@gamil.com>
2020-08-02 10:48:34 +0300

    calc

--- less_19/js/script.js
+++ less_19/js/script.js
@@ -238,5 +238,47 @@ window.addEventListener('DOMContentLoaded', function () {
             }
         }
     });
-    
+
+    //calc
+
+    let persons =   document.querySelectorAll('.counter-block-input')[0],
+        restDays =  document.querySelectorAll('.counter-block-input')[1],
+        place = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personsSum = 0,
+        daysSum = 0,
+        total = 0;
+
+        totalValue.innerHTML = 0;
+        persons.addEventListener('input', function(){            
+
+            if (restDays.value == '' || persons.value == ''){
+                totalValue.innerHTML = 0;
+            }else {
+                personsSum = +this.value;
+                total = (personsSum+daysSum)*450;
+                totalValue.innerHTML = total;
+            }
+        });
+
+        restDays.addEventListener('input', function(){
+            
+            if (persons.value == '' || restDays.value == ''){
+                totalValue.innerHTML = 0;
+            }else {
+                daysSum = +this.value;
+                total = (personsSum+daysSum)*450;
+                totalValue.innerHTML = total;
+            }
+        });
+
+
+        place.addEventListener('change', function(){
+            if (persons.value != '' || restDays.value != ''){
+                let a = total;
+                totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+            } else {
+                totalValue.innerHTML = 0;
+            }
+        })
 });
